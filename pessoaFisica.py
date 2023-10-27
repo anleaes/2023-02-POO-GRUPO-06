6a009ef357a19c0cb8817a15dfd1fd2ddbfd51ca
gtchorge <147110707+gtchorge@users.noreply.github.com>
2023-10-27 18:11:09 -0300

    ajustes

--- pessoaFisica.py
+++ pessoaFisica.py
@@ -4,6 +4,6 @@ class pessoaFisica(pessoa):
 
     def __int__(self, nome, endereco, cpf, data_nascimento):
         super() .__init__(nome, endereco)
-        self._cpf =cpf
+        self._cpf = cpf
         self._data_nascimento = data_nascimento
 
