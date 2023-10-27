7c2c4cccf75a20fc8eb3fead8767d1971ae14f9c
wilmagalhaes01 <115424208+wilmagalhaes01@users.noreply.github.com>
2023-10-26 21:46:40 -0300

    Create inscricao.py

---
+++ inscricao.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+class Inscricao:
+    def __init__(self, Numero, Data, Pessoa, Curso):
+        self.Numero = Numero
+        self.Data = Data
+        self.Pessoa = Pessoa
+        self.Curso = Curso
