395b70467ef9f80372cd245a725a161cccbaba80
wilmagalhaes01 <will.magalhaes01@gmail.com>
2023-10-26 21:36:03 -0300

    primeiraalteracao

--- inscricao.py
+++ inscricao.py
@@ -1,1 +1,8 @@
+from datetime import date
 
+class inscricao:
+    def __init__(self, Numero, Data, Pessoa, Curso):
+        self.Numero = Numero
+        self.Data = Data
+        self.Pessoa = Pessoa
+        self.Curso = Curso
