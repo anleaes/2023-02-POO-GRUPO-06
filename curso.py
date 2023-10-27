d46122bd6ad0f571a5a37a08480a8e1c8fa4f4cd
sant-matheus <mth1993santos@gmail.com>
2023-10-26 21:07:38 -0300

    addq

---
+++ curso.py
@@ -0,0 +1,11 @@
+class curso:
+
+    def __int__(self, nome, ementa, duracao, sala, professor):
+        self._nome = nome
+        self._ementa = ementa 
+        self._duracao = duracao
+        self._sala = sala
+        self._professor = professor 
+
+
+        
