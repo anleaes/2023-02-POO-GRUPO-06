7b9e7dbff7c60e99e4d1f0858aad486dc449e3da
wilmagalhaes01 <115424208+wilmagalhaes01@users.noreply.github.com>
2023-11-09 21:15:30 -0300

    Delete inscricao.py

--- inscricao.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-class Inscricao:
-    def __init__(self, Numero, Data, Pessoa, Curso):
-        self.Numero = Numero
-        self.Data = Data
-        self.Pessoa = Pessoa
-        self.Curso = Curso
