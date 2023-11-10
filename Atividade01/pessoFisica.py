6bb7a4170cd424650e744d1f29d370cae3e450c0
wilmagalhaes01 <115424208+wilmagalhaes01@users.noreply.github.com>
2023-11-09 21:13:10 -0300

    Delete Atividade01/pessoFisica.py

--- Atividade01/pessoFisica.py
+++
@@ -1,8 +0,0 @@
-from pessoa import pessoa
-
-class pessoaFisica(pessoa):
-
-    def __int__(self, nome, endereco, cpf, data_nascimento):
-        super() .__init__(nome, endereco)
-        self._cpf = cpf
-        self._data_nascimento = data_nascimento
