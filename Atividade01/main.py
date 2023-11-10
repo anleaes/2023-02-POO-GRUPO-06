3f62f5ebfea59069447f395316275928a2b7f80b
gtchorge <147110707+gtchorge@users.noreply.github.com>
2023-11-09 22:38:41 -0300

    main

--- Atividade01/main.py
+++ Atividade01/main.py
@@ -1,35 +1,23 @@
-class Pessoa{
-    - nome: String 
-    - endereco: String
-    + construtor(nome, endereco)
-}
+from datetime import date
+from pessoa import Pessoa
+from pessoaFisica import PessoaFisica
+from pessoaJuridica import PessoaJuridica
+from curso import Curso
+from matricula import Matricula
 
-class PessoaFisica{
-    - cpf: String
-    - data_nascimento: Date 
-    - pessoa: Pessoa
-    + construtor(cpf, data_nascimento, pessoa)
-}
+pessoa = Pessoa(nome="Fulano", endereco="Rua XYZ")
+pessoa2 = Pessoa(nome="Ciclano", endereco="Rua ABC")
+#print(f"Nome: {pessoa.nome}, Endereço: {pessoa.endereco}")
 
-class PessoaJuridica{
-    - cnpj: String
-    - razao_social: String
-    - data_abertura: Date
-    - pessoa: Pessoa
-    + construtor(cnpj, data_abertura, pessoa)
-}
+pessoa_fisica = PessoaFisica(cpf="123.456.789-01", data_nascimento=date(1990, 1, 1), pessoa=pessoa)
+#print(f"CPF: {pessoa_fisica.cpf}, Data de Nascimento: {pessoa_fisica.data_nascimento}, Nome: {pessoa_fisica.nome}")
 
-class Matricula{
-    -numero_matricula
-    -data_matricula
-    -curso: Curso
-    -aluno: PessoaFisica
-    -contratante: Pessoa 
-    + construtor(numero_matricula, data_matricula, curso, aluno, contratante)
-}
+pessoa_juridica = PessoaJuridica(cnpj="12.345.678/0001-90", razao_social="Empresa ABC", data_abertura=date(2000, 1, 1), pessoa=pessoa2)
+#print(f"CNPJ: {pessoa_juridica.cnpj}, Razão Social: {pessoa_juridica.razao_social}, Nome: {pessoa_juridica.nome}")
 
-class Curso {
-    -Nome String 
-    -carga_horaria String
-    + construtor(nome, carga_horaria)
-}
+curso = Curso(nome="Python Avançado", carga_horaria="40 horas")
+#print(f"Curso: {curso.nome}, Carga Horária: {curso.carga_horaria}")
+
+matricula = Matricula(numero_matricula=1, data_matricula=date.today(), curso=curso, aluno=pessoa_fisica, contratante=pessoa_juridica)
+print(f"Matrícula Nº: {matricula.numero_matricula}, Data de Matrícula: {matricula.data_matricula}")
+print(f"Curso: {matricula.curso.nome}, Aluno: {matricula.aluno.nome}, Contratante: {matricula.contratante.nome}")
