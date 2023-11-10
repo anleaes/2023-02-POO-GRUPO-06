3515bf49f452ef236e876ab6f592ea9c8153b9f9
wilmagalhaes01 <115424208+wilmagalhaes01@users.noreply.github.com>
2023-11-09 21:12:23 -0300

    Create pessoaJurídica.py

---
+++ Atividade01/pessoaJurídica.py
@@ -0,0 +1,9 @@
+from pessoa import pessoa
+
+class pessoaJuridica (pessoa):
+
+    def __int__(self, nome, endereco, cnpj, razao_social, data_abertura):
+        super() .__init__(nome, endereco)
+        self._cnpj = cnpj
+        self._razao_social = razao_social
+        self._data_abertura = data_abertura
