9fa47c97c7384b43fe993c050e1002ae82d3d686
wilmagalhaes01 <115424208+wilmagalhaes01@users.noreply.github.com>
2023-11-30 12:39:01 -0300

    Delete Atividade01 directory

--- Atividade01/pessoaFisica.py
+++
@@ -1,8 +0,0 @@
-from pessoa import Pessoa
-
-class PessoaFisica(Pessoa):
-    def __init__(self, cpf, data_nascimento, pessoa):
-        super().__init__(pessoa.nome, pessoa.endereco)
-        self.cpf = cpf
-        self.data_nascimento = data_nascimento
-        self.pessoa = pessoa
--- Atividade01/pessoaJuridica.py
+++
@@ -1,9 +0,0 @@
-from pessoa import Pessoa
-
-class PessoaJuridica(Pessoa):
-    def __init__(self, cnpj, razao_social, data_abertura, pessoa):
-        super().__init__(pessoa.nome, pessoa.endereco)
-        self.cnpj = cnpj
-        self.razao_social = razao_social
-        self.data_abertura = data_abertura
-        self.pessoa = pessoa
