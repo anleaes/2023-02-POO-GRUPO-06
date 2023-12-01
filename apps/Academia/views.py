81ef7b13c96e33ae9533f2304bd2a4e8169f36a0
wilmagalhaes01 <will.magalhaes01@gmail.com>
2023-11-30 21:32:59 -0300

    criação arquivo views

---
+++ apps/Academia/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Academia
+from rest_framework import viewsets
+from .serializer import AcademiaSerializer
+
+# Create your views here." 
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Academia.objects.all()
+    serializer_class = AcademiaSerializer 
