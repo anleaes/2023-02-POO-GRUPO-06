4be6bc7c1d4e4d186ef0e90556d83e8d8b0dda6d
wilmagalhaes01 <will.magalhaes01@gmail.com>
2023-12-07 11:34:11 -0300

    criacao branch correcoes finais

--- apps/planoalimentar/models.py
+++ apps/planoalimentar/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from Cliente.models import Cliente
 from Nutricionista.models import Nutricionista  
-from Alimentos.models import Alimentos
+from Alimento.models import Alimento
 
 # Create your models here.
 class Planoalimentar(models.Model):
@@ -9,7 +9,7 @@ class Planoalimentar(models.Model):
     
     
     
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     Nutricionista = models.ForeignKey(Nutricionista, on_delete=models.CASCADE)
     #Alimentos  = models.ForeignKey(Alimentos, on_delete=models.CASCADE)
 
